342ddea98233f3d2e130ae6a8ada2aca3f84b59e
Gabu <gabrielleon9928@gmail.com>
2025-11-14 20:01:26 +0300

    update

--- api/sendCode.js
+++ api/sendCode.js
@@ -4,13 +4,13 @@ export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(405).json({ message: "Method not allowed" });
 
-  const { email, code } = req.body;
+  const { email, verificationcode } = req.body;
 
   try {
     await Mail.sendMail(
       email,
-      "Your Verification Code",
-      { code },
+      "Verification Code",
+      verificationcode,
       "verification"
     );
     res.status(200).json({ success: true });

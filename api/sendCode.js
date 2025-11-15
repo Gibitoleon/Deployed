abf9b659dc3e7dfee6be1694491874251107acc0
Gabu <gabrielleon9928@gmail.com>
2025-11-15 12:46:56 +0300

    newchanges

--- api/sendCode.js
+++ api/sendCode.js
@@ -1,11 +1,28 @@
 const Mail = require("../Mail/Mail");
+const generateCode = require("../utils/otp");
+const db = require("../utils/dbconnection");
 
 export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(405).json({ message: "Method not allowed" });
 
-  const { email, verificationcode } = req.body;
+  const { email } = req.body;
+  // Generate a verification code
+  const verificationcode = generateCode();
 
+  // Save the code to  the firestore db associated with the user email
+  await db
+    .collection("verificationCodes")
+    .doc(email)
+    .set({
+      code: verificationcode,
+      createdAt: admin.firestore.Timestamp.now(),
+      expiresAt: admin.firestore.Timestamp.fromDate(
+        new Date(Date.now() + 5 * 60 * 1000)
+      ), // expires in 5 min
+    });
+
+  //Send the verifcation code to the user's email
   try {
     await Mail.sendMail(
       email,
